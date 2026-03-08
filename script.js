15f92fa6d97743b81a431bc3574c1f9011ea067e
GDEtrailrunning <gdeconcienciacorporal@gmail.com>
2026-03-08 13:24:04 -0300

    Subo semana 8 y 9

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ document.addEventListener("DOMContentLoaded", function () {
 const dataLine1 = {
   labels: ['Día 1', 'Día 2', 'Día 3', 'Día 4'], // Etiquetas de los días
   datasets: [{
-    label: 'Km recorridos en Semana 6',
-    data: [17, 9, 13, 21], // Datos de los km recorridos en cada día
+    label: 'Km recorridos en Semana 8',
+    data: [10, 9, 9, 26], // Datos de los km recorridos en cada día
     borderColor: 'rgba(75, 192, 192, 1)',
     fill: true,
     tension: 0.1
@@ -16,13 +16,14 @@ const dataLine1 = {
 const dataLine2 = {
   labels: ['Día 1', 'Día 2', 'Día 3', 'Día 4'], // Etiquetas de los días
   datasets: [{
-    label: 'Km recorridos en Semana 7',
-    data: [12, 15, 14, 14], // Datos de los km recorridos en cada día
+    label: 'Km recorridos en Semana 9',
+    data: [11, 10, 12, 27], // Datos de los km recorridos en cada día
     borderColor: 'rgba(153, 102, 255, 1)',
     fill: true,
     tension: 0.1
   }]
 };
+
 // Configuración del gráfico de líneas
 const configLine = {
   type: 'line',
